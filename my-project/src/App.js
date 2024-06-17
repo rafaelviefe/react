8ca9ac58dc3c8192a7cc981f0b2cf2b1f6d29402
Rafael Vieira Ferreira <161779899+rafaelviefe@users.noreply.github.com>
2024-06-16 23:51:09 -0300

    Update App.js

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 
 function App() {
+
+  const name = 'Rafael'
+
+  const newName = name.toUpperCase()
+
+  function sum(a, b){
+    return a + b
+  }
+
+  const url = "https://via.placeholder.com/150"
+
   return (
     <div className="App">
-      <h1>Olá React!</h1>
-      <p>Meu primeiro App</p>
+      <h2>Alterando o JSX</h2>
+      <p>Olá, {newName}</p>
+      <p>Soma: {sum(2, 2)}</p>
+      <img src={url} alt="Placeholder" />
     </div>
   );
 }
