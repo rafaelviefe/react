181a420e1bae2896dc5433e9f2099de682b8a42a
Rafael Vieira Ferreira <161779899+rafaelviefe@users.noreply.github.com>
2024-06-18 01:18:09 -0300

    useState

--- my-project/src/components/Forms.jsx
+++ my-project/src/components/Forms.jsx
@@ -1,16 +1,39 @@
+import { useState } from "react"
+
 function Form(){
     
     function registerUser(e){
         e.preventDefault()
+        console.log(`${name} and ${password}`)
         console.log("User registered.")
     }
     
+    const [name, setName] = useState()
+    const [password, setPassword] = useState()
+
     return (
         <div>
             <h1>My register:</h1>
             <form onSubmit={registerUser}>
                 <div>
-                    <input type="text" placeholder="write your name"/>
+                    <label htmlFor="name">Name:</label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        placeholder="write your name"
+                        onChange={(e)=> setName(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="write your password"
+                        onChange={(e)=> setPassword(e.target.value)}
+                    />
                 </div>
                 <div>
                     <input type="submit" value="register" />
