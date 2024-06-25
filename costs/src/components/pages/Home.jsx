1dd8e4485a818a7ad310d4d8cb78c53d103a99d5
Rafael Vieira Ferreira <161779899+rafaelviefe@users.noreply.github.com>
2024-06-24 21:05:00 -0300

    Creating dashbord

--- costs/src/components/pages/Home.jsx
+++ costs/src/components/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home(){
         <section className={styles.home_container}>
             <h1>Bem-vindo ao <span>Costs</span></h1>
             <p>Comece a gerenciar os seus projetos agora mesmo!</p>
-            <LinkButton to="newproject" text="Criar projeto" />
+            <LinkButton to="/newproject" text="Criar projeto" />
             <img src={savings} alt="Costs" />
         </section>
     )
