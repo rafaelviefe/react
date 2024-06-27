1df63e2f3b707c34c7f09aa7159a5ca291feb163
Rafael Vieira Ferreira <161779899+rafaelviefe@users.noreply.github.com>
2024-06-27 03:04:28 -0300

    Data edit page

--- costs/src/components/pages/Projects.jsx
+++ costs/src/components/pages/Projects.jsx
@@ -32,7 +32,6 @@ function Projects(){
             })
                 .then((resp) => resp.json())
                 .then((data) => {
-                    console.log(data)
                     setProjects(data)
                     setRemoveLoading(true)
                 })
